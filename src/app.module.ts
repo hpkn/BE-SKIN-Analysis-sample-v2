fe0a9f1c194861674f19e2c8c01f543f475e0fd3
hpkn <hassangpaul@gmail.com>
2024-03-05 11:06:29 +0900

    here we go

--- src/app.module.ts
+++ src/app.module.ts
@@ -51,6 +51,6 @@ export class AppModule {
     // Timing MiddleWare
     configure(consumer: MiddlewareConsumer) {
         consumer.apply(TimingMiddleware).forRoutes('*');
-        consumer.apply(AuthMiddleware).forRoutes('web-result/*');
+        // consumer.apply(AuthMiddleware).forRoutes('web-result/*');
     }
 }
