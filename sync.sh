a236021b4a613f1cba6f41c8080c205da17a8c3b
hpkn <hassangpaul@gmail.com>
2024-04-15 10:44:51 +0900

    here we go

---
+++ sync.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Set up the node environment
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
+
+echo "Starting deployment process"
+
+# Change directory to where your application is located
+cd /home/ubuntu/repositories/BE-CNDP-SKIN-v2/
+
+# Pull the latest changes from the Git repository
+echo "Pulling the latest changes from the Git repository"
+git pull origin main
+
+# RUN DB migration
+node run-migration.cjs
+
+# Install all the dependencies
+echo "Installing dependencies"
+
+source ~/.bashrc  # Refresh the shell to include updated PATH
+
+
+npm install
+
+# Build the application
+echo "Building application"
+npm run build
+
+# Restart the application using PM2
+echo "Restarting the application using PM2"
+pm2 restart ecosystem.config.js
+
+echo "Deployment process completed"
