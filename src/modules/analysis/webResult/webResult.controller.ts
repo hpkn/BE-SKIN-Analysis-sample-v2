bd572c3c848e79a90dbbb2b9bb963867e63f2d5d
hpkn <hassangpaul@gmail.com>
2024-04-16 12:20:26 +0900

    Web result expiration

--- src/modules/analysis/webResult/webResult.controller.ts
+++ src/modules/analysis/webResult/webResult.controller.ts
@@ -6,19 +6,22 @@ import { ApiTags } from '@nestjs/swagger';
 @ApiTags('WebResult')
 @Controller('web-result')
 export class WebResultController {
-    constructor(private readonly webResult: WebResultService) { }
+    constructor(private readonly webResult: WebResultService) {}
 
     @Get('/cndpskin/:batch_id')
-    async getBatchId(@Param('batch_id') batch_id: number, @Res() res: Response, @Query('check') checkExpiration: boolean) {
+    async getBatchId(
+        @Param('batch_id') batch_id: number,
+        @Res() res: Response,
+        @Query('check') checkExpiration: number,
+    ) {
         try {
-
             const requestDate = await this.webResult.getRequestDate(batch_id);
 
             if (!requestDate) {
                 await this.webResult.addRequestDate(batch_id);
             }
 
-            if (checkExpiration) {
+            if (this.webResult.numberToBoolean(Number(checkExpiration))) {
                 const isExpired = await this.webResult.checkExpiration(batch_id);
                 if (isExpired) {
                     return res.status(410).json({
@@ -40,4 +43,3 @@ export class WebResultController {
         }
     }
 }
-
--- src/modules/analysis/webResult/webResult.service.ts
+++ src/modules/analysis/webResult/webResult.service.ts
@@ -10,7 +10,7 @@ export class WebResultService {
         private database: DatabaseService,
         private readonly AlgoAnalysis: AlgoAnalysisService,
         private readonly computation: ComputationService,
-    ) { }
+    ) {}
 
     getSkinCondition(mScoreT: number, sScoreT: number, mScoreU: number, sScoreU: number, sebumQAScore: number) {
         const veryDry = 1;
@@ -469,11 +469,9 @@ export class WebResultService {
     }
 
     async checkExpiration(batch_id: number) {
-
-        const result = await this.database.executeQuery(
-            `SELECT request_date FROM analysis WHERE batch_id = $1`,
-            [batch_id],
-        )
+        const result = await this.database.executeQuery(`SELECT request_date FROM analysis WHERE batch_id = $1`, [
+            batch_id,
+        ]);
 
         if (result.length === 0) {
             return true;
@@ -488,22 +486,29 @@ export class WebResultService {
     }
 
     async getRequestDate(batch_id: number) {
-
-        const result = await this.database.executeQuery(
-            `SELECT request_date FROM analysis WHERE batch_id = $1`,
-            [batch_id],
-        )
+        const result = await this.database.executeQuery(`SELECT request_date FROM analysis WHERE batch_id = $1`, [
+            batch_id,
+        ]);
 
         return result[0].request_date;
     }
 
     async addRequestDate(batch_id: number) {
-
-        const result = await this.database.executeQuery(
-            `UPDATE analysis SET request_date = $1 WHERE batch_id = $2`,
-            [new Date(), batch_id],
-        )
+        const result = await this.database.executeQuery(`UPDATE analysis SET request_date = $1 WHERE batch_id = $2`, [
+            new Date(),
+            batch_id,
+        ]);
 
         return result;
+    }
+
+    numberToBoolean(number: any) {
+        if (number === 1) {
+            return true;
+        } else if (number === 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
