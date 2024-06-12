26fe48b68d64e8e8c59cea7e93769364445743a9
hpkn <hassangpaul@gmail.com>
2024-06-12 16:08:07 +0900

    push to stg_main

--- src/modules/analysis/algoAnalysis/algoAnalysis.service.ts
+++ src/modules/analysis/algoAnalysis/algoAnalysis.service.ts
@@ -2252,6 +2252,7 @@ export class AlgoAnalysisService {
                     ultra_fine_score: algoId === 4 && data?.ultraFineScore ? data?.ultraFineScore[i] : null,
                     deep_score: algoId === 4 && data?.deepScore ? data?.deepScore[i] : null,
                     ultra_deep_score: algoId === 4 && data?.ultraDeepScore ? data?.ultraDeepScore[i] : null,
+                    sebumType: data?.sebumType ? data?.sebumType : null,
                 }),
             ]);
 
