2386ac890b924c49408ded9b7d77bbe8c7c0f3b1
hpkn <hassangpaul@gmail.com>
2024-09-09 18:06:45 +0900

    query with time

--- src/common/Dto/customer/analysisHistory/analysisHistory.dto.ts
+++ src/common/Dto/customer/analysisHistory/analysisHistory.dto.ts
@@ -2,40 +2,38 @@ import { IsNumber, Min, IsOptional, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class GetcustomerHistoryDTO {
-  @IsOptional()
-  @Type(() => Number)
-  @IsNumber()
-  customer_id?: number;
+    @IsOptional()
+    @Type(() => Number)
+    @IsNumber()
+    customer_id?: number;
 
-  @IsOptional()
-  @Type(() => Number)
-  @IsNumber()
-  page?: number;
+    @IsOptional()
+    @Type(() => Number)
+    @IsNumber()
+    page?: number;
 
-  @IsOptional()
-  @Type(() => Number)
-  @IsNumber()
-  limit?: number;
+    @IsOptional()
+    @Type(() => Number)
+    @IsNumber()
+    limit?: number;
 
-  @IsOptional()
-  @Type(() => Number)
-  @IsString()
-  from?: String;
+    @IsOptional()
+    @Type(() => String)
+    from?: String;
 
-  @IsOptional()
-  @Type(() => Number)
-  @IsString()
-  to?: String;
+    @IsOptional()
+    @Type(() => String)
+    to?: String;
 
-  @IsOptional()
-  @Type(() => Number)
-  @IsString()
-  search?: number;
+    @IsOptional()
+    @Type(() => Number)
+    @IsString()
+    search?: number;
 
-  @IsOptional()
-  @IsString()
-  filter_by?: string;
+    @IsOptional()
+    @IsString()
+    filter_by?: string;
 
-  @IsOptional()
-  batch_id?: number | string;
+    @IsOptional()
+    batch_id?: number | string;
 }
