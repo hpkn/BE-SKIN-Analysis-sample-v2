0def3fa29687a55db4e5427196f06cf0207d17db
hpkn <hassangpaul@gmail.com>
2024-09-09 18:07:44 +0900

    query with time

--- src/modules/customer/customerHistory/customerHistory.controller.ts
+++ src/modules/customer/customerHistory/customerHistory.controller.ts
@@ -29,7 +29,6 @@ export class AnanalysisHistoryController {
         @Query() query: GetcustomerHistoryDTO,
         @Res() res: Response,
     ) {
-        console.log('query --->', query);
         const result = await this.getAnalysisHistory.GetcustomerHistory(customer_id, query);
 
         return res.status(200).send(result);
